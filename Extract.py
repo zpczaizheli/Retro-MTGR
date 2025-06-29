6286c36d26e65b28c59174dca09a2114b544041f
zpczaizheli <47655168+zpczaizheli@users.noreply.github.com>
2025-06-29 15:37:49 +0800

    Add files via upload

---
+++ Extract.py
@@ -0,0 +1,262 @@
+
+from rdkit import Chem
+from rdkit.Chem import Draw
+if_img = False
+
+step = 2
+def removeHs(smiles):
+    atoms = smiles.split(']')
+    smart = ''
+    for atom in atoms:
+        if len(atom.split(':')) < 2:
+            if len(atom.split('[')) < 2:
+                smart += atom
+
+            if len(atom.split('[')) == 2:
+                smart += atom + ']'
+            continue
+
+        w1, w2 = atom.split(':')
+        num = 0
+        if len(w1) < 3:
+            for i in range(len(w1)-2, len(w1)):
+                if w1[i] == 'H':
+                    num = i
+                    break
+        else:
+            for i in range(len(w1)-3, len(w1)):
+                if w1[i] == 'H':
+                    num = i
+                    break
+        if num != 0:
+            smart += atom[0:num] + ':' + w2
+        else:
+            smart += atom
+        smart += ']'
+    return smart
+
+
+def complete_ring(Index_list, smiles1):
+
+    mol = Chem.MolFromSmiles(smiles1)
+    mid_list = Index_list[:]
+    aromatic_atoms = [atom.GetIdx() for atom in mol.GetAtoms() if atom.GetIsAromatic()]
+    Ring_atom_index = []
+    for index in mid_list:
+        if index in aromatic_atoms:
+            Ring_atom_index.append(index)
+
+    ring_info = mol.GetRingInfo()
+    num_rings = ring_info.NumRings()
+
+    for index1 in Ring_atom_index:
+        for index2 in range(mol.GetNumAtoms()):
+            for i in range(num_rings):
+                ring_atoms = ring_info.AtomRings()[i]
+                if index1 in ring_atoms and index2 in ring_atoms:
+                    mid_list.append(index2)
+                    break
+    return list(set(mid_list))
+
+
+def SymbolAtom(mol):
+    Atomnum = mol.GetNumAtoms()
+    for i in range(Atomnum):
+        Atom = mol.GetAtomWithIdx(i)
+        Atom.SetProp("atomNote", str(i))
+    return mol
+
+
+def Get_changed_atoms_map(reaction):
+    reactants,products = reaction.split('>>')
+    Changed_Atoms_map = []
+    for reactant in reactants.split('.'):
+
+        Mol_reactant = Chem.MolFromSmiles(reactant)
+        num_atoms_R = Mol_reactant.GetNumAtoms()
+        for atom_idx in range(num_atoms_R):
+            Condition_list_R = {}
+            atom = Mol_reactant.GetAtomWithIdx(atom_idx)
+            atom_map = atom.GetAtomMapNum()
+
+            neighbor_atoms = atom.GetNeighbors()
+            neighbor_map = []
+
+            for neighbor_atom in neighbor_atoms:
+                neighbor_map.append(neighbor_atom.GetAtomMapNum())
+            Condition_list_R[atom_map] = neighbor_map
+
+            x = False
+            for product in products.split('.'):
+                Mol_product = Chem.MolFromSmiles(product)
+                num_atoms_P = Mol_product.GetNumAtoms()
+                for i in range(num_atoms_P):
+                    Condition_list_P = {}
+                    atom_P = Mol_product.GetAtomWithIdx(i)
+                    atom_map_P = atom_P.GetAtomMapNum()
+                    neighbor_atoms_P = atom_P.GetNeighbors()
+                    neighbor_map_P = []
+                    for neighbor_atom_P in neighbor_atoms_P:
+                        neighbor_map_P.append(neighbor_atom_P.GetAtomMapNum())
+                    Condition_list_P[atom_map_P] = neighbor_map_P
+
+                    if set(Condition_list_P[atom_map_P]) == set(Condition_list_R[atom_map]) and atom_map_P == atom_map:
+
+                        x = True
+                        break
+                if x:
+                    break
+            if x == False:
+                Changed_Atoms_map.append(atom_map)
+
+    return Changed_Atoms_map
+
+
+def Get_Reaction_t(reaction,step):
+    Changed_Atoms_map = Get_changed_atoms_map(reaction)
+    Save_atom_Index_list_R = []
+    Save_atom_Index_list_P = []
+    reactants, products = reaction.split('>>')
+    tem = ''
+    for reactant in reactants.split('.'):
+        Mol_reactant = Chem.MolFromSmiles(reactant)
+
+        if if_img:
+            img = Draw.MolToImage(SymbolAtom(Mol_reactant))
+            img.show()
+        num_atoms_R = Mol_reactant.GetNumAtoms()
+        Index_list = []
+        for atom_idx in range(num_atoms_R):
+            mid_list = []
+            atom = Mol_reactant.GetAtomWithIdx(atom_idx)
+            atom_map = atom.GetAtomMapNum()
+            if atom_map in Changed_Atoms_map:
+
+                mid_list.append(atom_idx)
+                for i in range(step):
+                    fin_list = mid_list[:]
+                    for j in fin_list:
+                        ato = Mol_reactant.GetAtomWithIdx(j)
+                        neighbor_atoms = ato.GetNeighbors()
+                        neighbor_index_list = []
+                        for neighbor_atom in neighbor_atoms:
+                            neighbor_index_list.append(neighbor_atom.GetIdx())
+                        mid_list += neighbor_index_list
+                mid_list = list(set(mid_list))
+            else:
+                continue
+            Index_list += mid_list
+            Index_list = list(set(Index_list))
+
+        Index_list = complete_ring(Index_list, reactant)
+
+        Save_atom_Index_list_R.append(Index_list)
+
+    for product in products.split('.'):
+        Mol_product = Chem.MolFromSmiles(product)
+        if if_img:
+            img = Draw.MolToImage(SymbolAtom(Mol_product))
+            img.show()
+        num_atoms_P = Mol_product.GetNumAtoms()
+        Index_list = []
+        for atom_idx in range(num_atoms_P):
+            mid_list = []
+            atom = Mol_product.GetAtomWithIdx(atom_idx)
+            atom_map = atom.GetAtomMapNum()
+
+            if atom_map == 0:
+                Index_list.append(atom_idx)
+                continue
+            if atom_map in Changed_Atoms_map:
+
+                mid_list.append(atom_idx)
+                for i in range(step):
+                    fin_list = mid_list[:]
+                    for j in fin_list:
+                        ato = Mol_product.GetAtomWithIdx(j)
+                        neighbor_atoms = ato.GetNeighbors()
+                        neighbor_index_list = []
+                        for neighbor_atom in neighbor_atoms:
+                            neighbor_index_list.append(neighbor_atom.GetIdx())
+                        mid_list += neighbor_index_list
+                mid_list = list(set(mid_list))
+            else:
+                continue
+            Index_list += mid_list
+            Index_list = list(set(Index_list))
+        Index_list = complete_ring(Index_list, product)
+        Save_atom_Index_list_P.append(Index_list)
+
+    Mol_Xing = Chem.MolFromSmiles('*')
+    for i in range(len(reactants.split('.'))):
+        save_index = Save_atom_Index_list_R[i]
+        Mol_reactant = Chem.MolFromSmiles(reactants.split('.')[i])
+        num_atoms_R = Mol_reactant.GetNumAtoms()
+        mw = Chem.RWMol(Mol_reactant)
+        for atom_idx in range(num_atoms_R):
+            if atom_idx in save_index:
+                continue
+            else:
+                mw.ReplaceAtom(atom_idx, Chem.Atom(0))
+        m = mw.GetMol()
+        Mm = Chem.DeleteSubstructs(m, Mol_Xing)
+
+        if if_img:
+            img = Draw.MolToImage(Mm)
+            img.show()
+
+        tem += removeHs(Chem.MolToSmiles(Mm))
+        if i < len(reactants.split('.'))-1:
+            tem += '.'
+    tem +='>>'
+    for i in range(len(products.split('.'))):
+        save_index = Save_atom_Index_list_P[i]
+        Mol_product = Chem.MolFromSmiles(products.split('.')[i])
+        num_atoms_P = Mol_product.GetNumAtoms()
+        mw = Chem.RWMol(Mol_product)
+        for atom_idx in range(num_atoms_P):
+            if atom_idx in save_index:
+                continue
+            else:
+                mw.ReplaceAtom(atom_idx, Chem.Atom(0))
+        m = mw.GetMol()
+
+        Mm = Chem.DeleteSubstructs(m, Mol_Xing)
+
+        if if_img:
+            img = Draw.MolToImage(Mm)
+            img.show()
+
+        tem += removeHs(Chem.MolToSmiles(Mm))
+        if i < len(products.split('.'))-1:
+            tem += '.'
+    return tem
+
+def reaction_weather_equal(reaction1,reaction2):
+    products1, reactants1 = reaction1.split('>>')
+    products2, reactants2 = reaction2.split('>>')
+    mol_products1 = Chem.MolFromSmarts(products1)
+    mol_products2 = Chem.MolFromSmarts(products2)
+    mol_reactants1 = Chem.MolFromSmarts(reactants1)
+    mol_reactants2 = Chem.MolFromSmarts(reactants2)
+    X = False
+    if mol_products1 and mol_products2 and mol_reactants1 and mol_reactants2:
+        if mol_products1.HasSubstructMatch(mol_products2) and mol_products2.HasSubstructMatch(mol_products1):
+            if mol_reactants1.HasSubstructMatch(mol_reactants2) and mol_reactants2.HasSubstructMatch(mol_reactants1):
+                X = True
+    else:
+        print(reaction1)
+        print(reaction2)
+    return X
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ model3.py
@@ -0,0 +1,140 @@
+
+
+from Model_AFPNN_Retrosynthesis_5 import *
+
+def remove_zero_rows_and_columns(matrix):
+    if not matrix:
+        return matrix
+
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+
+    zero_rows = set()
+    zero_cols = set()
+
+    for row in range(num_rows):
+        for col in range(num_cols):
+            if matrix[row][col] != 0:
+                break
+        else:
+            zero_rows.add(row)
+
+
+    for col in range(num_cols):
+        for row in range(num_rows):
+            if matrix[row][col] != 0:
+                break
+        else:
+            zero_cols.add(col)
+
+    new_matrix = [matrix[i] for i in range(num_rows) if i not in zero_rows]
+    new_matrix = [[new_matrix[i][j] for j in range(num_cols) if j not in zero_cols] for i in range(len(new_matrix))]
+
+    return new_matrix
+
+
+def Listadd(list1, list2):
+	result = []
+	for i in range(len(list1)):
+		result.append(list1[i] + list2[i])
+	return result
+
+
+def GetLabelsrelationship(Labels,Labels_dict_OneHot,Data):
+	LabelsDict_relasionship = {}
+	for i in range(len(Labels)):
+		LabelsDict_relasionship[Labels[i]] = [0.0]*len(Labels)
+	for num in range(len(Data[0])):
+		Smiles_P = Data[0][str(num)]
+		Smiles_R1 = Data[1][str(num)]
+		Smiles_R2 = Data[2][str(num)]
+		BreakBondID = Data[3][str(num)]
+		data = [Smiles_P, BreakBondID, Smiles_R1, Smiles_R2]
+		label1, label2 = Data_Processing(data, Labels_dict_OneHot)
+		x = 0
+		y = 0
+		for i in range(len(label1)):
+			if label1[i] == 1:
+				x = i
+			if label2[i] == 1:
+				y = i
+		LabelsDict_relasionship[Labels[x]] = Listadd(LabelsDict_relasionship[Labels[x]],label2)
+		LabelsDict_relasionship[Labels[y]] = Listadd(LabelsDict_relasionship[Labels[y]],label1)
+	
+
+	Adj_Labels = []
+	mini_labels = []
+	for num in range(len(Labels)):
+		if sum(LabelsDict_relasionship[Labels[num]]) != 0:
+			mini_labels.append(Labels[num])
+		Adj_Labels.append(LabelsDict_relasionship[Labels[num]])
+
+	matrix = remove_zero_rows_and_columns(Adj_Labels)
+
+	mini_adj = []
+	mini_Labels_dict = {}
+
+	# coun = 1
+	for coun in range(len(mini_labels)):
+		row = [0.0]*(coun) +  [1.0] + [0.0]*(len(mini_labels) - coun - 1)
+		if mini_labels[coun] not in mini_Labels_dict.keys():
+			mini_Labels_dict[mini_labels[coun]] = row
+		mini_adj.append(row)
+	v = 0
+	result = []
+	for i in matrix:
+		normalized_list = [x / sum(i) for x in i]
+		count = sum(i)
+		result.append(normalized_list)
+		v += count
+		print(i,count)
+
+	return mini_Labels_dict, mini_labels,len(mini_labels),mini_adj,result
+
+class GraphConvolution(nn.Module):
+	def __init__(self, in_features, out_features, device, bias=True):
+		super(GraphConvolution, self).__init__()
+		self.weight = nn.Linear(in_features, out_features).to(device)
+		self.use_bias = bias
+		if self.use_bias:
+			self.bias = nn.Parameter(torch.FloatTensor(out_features)).to(device)
+		self.reset_parameters()
+		
+	def reset_parameters(self):
+		#nn.init.normal_(self.weight)
+		#nn.init.kaiming_uniform_(self.weight)
+		if self.use_bias:
+			nn.init.zeros_(self.bias)
+		pass
+	
+	def forward(self, input_features, adj):
+		support = self.weight(input_features)
+		output = torch.mm(adj, support)
+		output = torch.tanh(output)
+		#print('output:',output)
+		if self.use_bias: 
+			#print(self.bias)
+			return output + self.bias
+		else:
+			return output
+
+
+class Model3(nn.Module):
+	def __init__(self,input_dim,output_dim,device):
+		super(Model3, self).__init__()
+		self.gcn1 = GraphConvolution(input_dim, input_dim * 2,device)
+		self.gcn2 = GraphConvolution(input_dim * 2, output_dim,device)
+		#self.fc1 = nn.Linear(56, 28).to(device)
+		self.reset_parameters()
+		pass
+	def reset_parameters(self):
+		#nn.init.normal_(self.weight_FC)
+		#nn.init.normal_(self.weight_BondEnergy)
+		#nn.init.kaiming_uniform_(self.weight_BondEnergy)
+		pass
+	def forward(self,Nodes_Feature,Adj):
+		output = self.gcn1(Nodes_Feature,Adj)
+		#output = torch.relu(output)
+		output = self.gcn2(output,Adj)
+		return output
+
